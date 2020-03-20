b46c4e90c8f03ca519613401364cc491ed7ff8be
Ananta Satya Dhamma <satyadhammaananta@gmail.com>
2020-03-20 16:10:58 +0700

    Tugas 1 OOP1 Ananta Satya Dhamma 18090003

---
+++ src/oop1/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop1;
+
+/**
+ *
+ * @author Anan
+ */
+public class Mahasiswa {
+    String nama;
+    String kelas;
+    String nim;
+    
+    public void cetak(){
+        System.out.println(nama);
+        System.out.println(kelas);
+        System.out.println(nim);
+    }
+}
---
+++ src/oop1/Profil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop1;
+
+/**
+ *
+ * @author Anan
+ */
+public class Profil {
+    public static void main(String[]args){
+     Mahasiswa mhs = new Mahasiswa();
+     
+     mhs.nama = "Ananta Satya Dhamma";
+     mhs.kelas = "4C";
+     mhs.nim = "18090003";
+     
+     mhs.cetak();
+    }
+}
